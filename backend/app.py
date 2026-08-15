1915842c0ab3a2d6d1964b3414d3a14cd229ade3
niharika96915 <niharikalakhera86@gmail.com>
2026-08-15 09:02:48 +0530

    Update cors configuration for vercel

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://ai-traffic-violation-detection-one.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
